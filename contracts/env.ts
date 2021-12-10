d64275410873399cc2305c56f65fcf8ab4f4ca46
Hannah Wolfe <github.erisds@gmail.com>
2021-12-10 12:28:19 +0000

    Configure lucid/database

--- contracts/env.ts
+++ contracts/env.ts
@@ -18,7 +18,19 @@ declare module '@ioc:Adonis/Core/Env' {
   |
   */
 
-	type CustomTypes = typeof import("../env").default;
-  interface EnvTypes extends CustomTypes {
-  }
+	type CustomTypes = typeof import('../env').default
+	interface EnvTypes extends CustomTypes {
+
+	}
 }
+
+import Env from '@ioc:Adonis/Core/Env'
+
+export default Env.rules({
+  DB_CONNECTION: Env.schema.string(),
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
+})
